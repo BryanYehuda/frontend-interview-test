121b09651af74f80c4878bbf6f7981789f778199
Bryan Yehuda <bryanyehuda@gmail.com>
2023-01-16 00:10:31 +0700

    Finishing Project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const editor = document.getElementById('showEditor');
 const size = document.getElementById("showSize");
 const slider = document.getElementById('slider');
 const color = document.getElementById('colorEditor');
+const background = document.getElementById('backgroundEditor');
 
 editor.addEventListener('input', (event) => {
   preview.textContent = event.target.value;
@@ -19,10 +20,25 @@ color.addEventListener('input', (event) => {
   preview.style.color = event.target.value;
 })
 
+background.addEventListener('input', (event) => {
+  preview.style.backgroundColor = event.target.value;
+})
+
  function displayDefault(){
   logoName = "LogoMaker";
   logoSize = "25px";
   preview.innerHTML = logoName;
   size.innerHTML = "Logo Size is " + logoSize;
+}
+
+function downloadimage() {
+  html2canvas(preview, { allowTaint: true }).then(function (canvas) {
+      var link = document.createElement("a");
+      document.body.appendChild(link);
+      link.download = "logo.jpg";
+      link.href = canvas.toDataURL();
+      link.target = '_blank';
+      link.click();
+  });
 }
 
